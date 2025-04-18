7dfa59a83c6631900099de490a5aacdcd9eacfbe
amelia-liston <aliston@bu.edu>
2025-04-18 15:12:27 -0400

    More styling updates

--- app/components/NewLinkForm.tsx
+++ app/components/NewLinkForm.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { Textarea } from "@mui/joy";
 import { Button, TextField } from "@mui/material";
 import { useState } from "react";
 import { LinkProps } from "@/types";
 import createNewLink from "@/app/lib/createNewLink";
+import Container from '@mui/material/Container';
 
 export default function NewLinkForm(){
     const [url, setUrl] = useState("");
@@ -12,55 +12,55 @@ export default function NewLinkForm(){
     const [linkProp, setLinkProps] = useState<LinkProps>();
     return(
         <>
-            <form
-                style={{width: "60%", margin: "0 auto"}}
-                onSubmit={async (e)=>{
-                e.preventDefault();
-                const res = await createNewLink(url, alias);
-                if (typeof res == 'string' && res === "Invalid URL") {
-                    setErrorMessage("Invalid URL, please try again with a new URL");
-                } else if (typeof res == 'string' && res === "This alias already exists") {
-                    setErrorMessage(res);
-                } else if (typeof res !== 'string') {
-                    setLinkProps(res);
-                    setErrorMessage("");
-                }
-                }}>
-                <h2 className="text-xl mt-2 mb-2">URL</h2>
-                <TextField
-                    variant="filled"
-                    sx={{backgroundColor: "white", width: "100%"}}
-                    label="https://example.com/very/long/url"
-                    value={url}
-                    onChange={(e)=> setUrl(e.target.value)}
-                />
-                <h2 className="text-xl mt-2 mb-2">Custom Alias</h2>
-                <Textarea
-                    sx={{
-                        padding: "0.5rem",
-                        height: "100px",
-                        width: "100%",
-                        borderRadius: 0,
-                    }}
-                    variant="soft"
-                    placeholder="your-custom-alias"
-                    value={alias}
-                    onChange={(e)=> setAlias(e.target.value)}
-                />
-                <div>
-                    <Button type="submit" variant="contained" sx={{width: "100%"}}>
-                        Shorten
-                    </Button>
+            <Container maxWidth="md" className="bg-white drop-shadow-2xl rounded-xl mb-10">
+                <form
+                    style={{width: "90%", margin: "0 auto"}}
+                    onSubmit={async (e)=>{
+                    e.preventDefault();
+                    const res = await createNewLink(url, alias);
+                    if (typeof res == 'string' && res === "Invalid URL") {
+                        setErrorMessage("Invalid URL, please try again with a new URL");
+                    } else if (typeof res == 'string' && res === "This alias already exists") {
+                        setErrorMessage(res);
+                    } else if (typeof res !== 'string') {
+                        setLinkProps(res);
+                        setErrorMessage("");
+                    }
+                    }}>
+                    <h2 className="text-xl mt-6 mb-2">URL</h2>
+                    <TextField
+                        variant="filled"
+                        sx={{backgroundColor: "white", width: "100%"}}
+                        label="https://example.com/very/long/url"
+                        value={url}
+                        onChange={(e)=> setUrl(e.target.value)}
+                    />
+                    <h2 className="text-xl mt-2 mb-2">Custom Alias</h2>
+                    <TextField
+                        variant="filled"
+                        sx={{backgroundColor: "white", width: "100%"}}
+                        label="your-custom-alias"
+                        value={alias}
+                        onChange={(e)=> setAlias(e.target.value)}
+                    />
+                    <div className="mt-6 mb-6">
+                        <Button type="submit" variant="contained" sx={{width: "100%", backgroundColor: "purple"}}>
+                            Shorten
+                        </Button>
+                    </div>
+                </form>
+                <div className="flex justify-center mb-4">
+                    { (errorMessage !== "" || !linkProp)
+                        ?(
+                            <div className="text-2xl text-red-700 font-bold">{errorMessage}</div>
+                        )
+                        :<div  className="text-2xl text-black">
+                            <a href={`${window.location.origin}/${linkProp.alias}`}>Your new url: <b>{`${window.location.origin}/${linkProp.alias}`}</b></a>
+                        </div>
+                    }
                 </div>
-            </form>
-            { (errorMessage !== "" || !linkProp)
-                ?(
-                    <div>{errorMessage}</div>
-                )
-                :<div>
-                    <a href={`${window.location.origin}/${linkProp.alias}`}>{`${window.location.origin}/${linkProp.alias}`}</a>
-                </div>
-            }
+
+            </Container>
         </>
     )
 }
