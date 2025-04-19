235625a874bbfbe1eb5e6dc8eda59b1ab9330a0c
amelia-liston <aliston@bu.edu>
2025-04-18 20:16:36 -0400

    polishing up

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col items-center bg-purple-200 p-4">
-          <h1 className="text-3xl mt-6 mb-4">URL Shortener</h1>
+          <h1 className="text-3xl mt-6 mb-4 font-bold">URL Shortener</h1>
           <h2 className="text-2xl mb-4">Shorten your long urls to compact, shareable links</h2>
         <NewLinkForm />
       </div>
